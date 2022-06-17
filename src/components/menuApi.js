65cae784736282776b97d3d94f331c40ff460983
Nirav1424 <niravasodariya7787@gmail.com>
2022-06-17 22:42:18 +0530

    solve img pb

--- src/components/menuApi.js
+++ src/components/menuApi.js
@@ -1,8 +1,8 @@
 const Menu = [
   {
     id: 1,
-    image: "images/maggi.jpg",
-    name: "maggi",
+    image: "https://i.ytimg.com/vi/NLBN2WG3DKU/maxresdefault.jpg",
+    name: "fenki",
     category: "breakfast",
     price: "12₹",
     description:
@@ -11,8 +11,8 @@ const Menu = [
 
   {
     id: 2,
-    image: "images/allupakoida.jpg",
-    name: "allu pakoida",
+    image: "https://t3.ftcdn.net/jpg/00/54/60/80/360_F_54608019_A2KwTv0JqTUZhTW1GQ4t1RNBNl2QAvfB.jpg",
+    name: "puf",
     category: "evening",
     price: "20₹",
     description:
@@ -20,17 +20,17 @@ const Menu = [
   },
   {
     id: 3,
-    image: "images/corn.jpg",
-    name: "corn",
+    image: "https://5.imimg.com/data5/UH/QO/MY-53670282/plain-maggi-500x500.png",
+    name: "megi",
     category: "breakfast",
     price: "10₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
+      "only 2 min to make consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
   },
   {
     id: 4,
-    image: "../images/chola.jpg",
-    name: "chola",
+    image: "https://i.ytimg.com/vi/JVZ4U_r0GBo/maxresdefault.jpg",
+    name: "bhel",
     category: "lunch",
     price: "50₹",
     description:
@@ -38,7 +38,7 @@ const Menu = [
   },
   {
     id: 5,
-    image: "../images/pizza.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn7mEzHXTd3wwflVKMQJkT0iugrpAahf039dEuj35s2iPu5RT7PctmyJmuXBMKnXEm5KQ&usqp=CAU",
     name: "pizza",
     category: "evening",
     price: "80₹",
@@ -47,17 +47,17 @@ const Menu = [
   },
   {
     id: 6,
-    image: "../images/nonvegthali.jpg",
-    name: "Non-Veg Thali",
-    category: "dinner",
+    image: "https://mitalideliciouskitchen.com/wp-content/uploads/2020/06/Aloo-puri-Main-Pic-.jpg",
+    name: "alu puri",
+    category: "evening",
     price: "180₹",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
   },
   {
     id: 7,
-    image: "../images/sweet.jpg",
-    name: "Sweets",
+    image: "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/b/b/p71-16151850316045c48705b80.jpg?tr=tr:n-xlarge",
+    name: "burger",
     category: "dinner",
     price: "60₹",
     description:
@@ -65,16 +65,16 @@ const Menu = [
   },
   {
     id: 8,
-    image: "../images/rajmarice.jpg",
-    name: "Rajma Rice",
-    category: "lunch",
+    image: "https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/glnraqp4kbvpy1smoui2",
+    name: "vadapav ",
+    category: "evening",
     price: "60₹",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
   },
   {
     id: 9,
-    image: "../images/samosa.jpg",
+    image: "https://www.kindpng.com/picc/m/557-5575090_large-samosa-png-download-veg-samosa-transparent-png.png",
     name: "samaso",
     category: "evening",
     price: "10₹",
@@ -83,12 +83,12 @@ const Menu = [
   },
   {
     id: 10,
-    image: "../images/panipuri.png",
+    image: "https://www.pngitem.com/pimgs/m/218-2187931_pani-puri-images-png-transparent-png.png",
     name: "panipuri",
     category: "evening",
     price: "10₹",
     description:
-      "specialfor my friends r adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
+      "special for my wife roshni adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto.  ",
   },
 ];
 
